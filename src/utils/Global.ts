6802f4a8218ee06df8945d8063ffdb331d5b3707
Apricityx <apricityx@qq.com>
2025-04-29 21:37:40 +0800

    fix. New backend.

--- src/utils/Global.ts
+++ src/utils/Global.ts
@@ -1,4 +1,4 @@
-export const backendUrl = 'https://tour.apricityx.top:8000'
+export const backendUrl = 'https://virtour.apricityx.top:8000'
 
 export const blobToBase64 = (blob: Blob) => {
   return new Promise((resolve, reject) => {
